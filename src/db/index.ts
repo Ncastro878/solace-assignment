db27a79ba3991b88205631fa9650e4f5f196b0ec
Nick Castro <ncastro878@gmail.com>
2025-04-13 02:41:33 -0500

    wip

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,20 +1,37 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
+import { drizzle } from 'drizzle-orm/postgres-js'
+import postgres from 'postgres'
 
 const setup = () => {
-  if (!process.env.DATABASE_URL) {
-    console.error("DATABASE_URL is not set");
+  // Hacky temp workaround so the mock db can chain limit(), offset(), where() when envvars not set
+  // This helps avoid type errors during development and allows us to 'run npm run build'
+  // TODO: Refactor a cleaner mock - possibly use envvars to check if local or prod instead
+  if (!process.env.DATABASE_URL || process.env.MOCK_DB_DATA) {
+    console.error('DATABASE_URL is not set or MOCK_DB_DATA is true')
     return {
-      select: () => ({
-        from: () => [],
+      select: (q?: any) => ({
+        from: () => ({
+          limit: (n: number) => ({
+            offset: (m: number) => [],
+          }),
+          where: (q?: any) => ({
+            limit: (n: number) => ({
+              offset: (m: number) => [],
+            }),
+          }),
+        }),
       }),
-    };
+      insert: (q?: any) => ({
+        values: (q?: any) => ({
+          returning: (q?: any) => [],
+        }),
+      }),
+    }
   }
 
   // for query purposes
-  const queryClient = postgres(process.env.DATABASE_URL);
-  const db = drizzle(queryClient);
-  return db;
-};
+  const queryClient = postgres(process.env.DATABASE_URL)
+  const db = drizzle(queryClient)
+  return db
+}
 
-export default setup();
+export default setup()

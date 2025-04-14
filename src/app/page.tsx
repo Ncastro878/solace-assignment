07628589c07c02a1d3da0481ddb6b307ff264802
Nick Castro <ncastro878@gmail.com>
2025-04-13 21:43:01 -0500

    visual styling updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,10 @@ import { getAdvocates } from "./services/advocatesService";
 export default async function Home() {
   const { data: initialAdvocates, totalCount } = await getAdvocates(dbDefaultLimit, dbDefaultOffset);
   return (
-    <main style={{ margin: "24px" }}>
-      <h1>Solace Advocates</h1>
+    <main style={{ margin: "24px", fontFamily: "sans-serif" }}>
+      <div className="flex items-center justify-center border-b-2 border-[#285e50] pb-[10px]">
+        <h1 className="text-[#285e50] font-bold text-2xl">Solace Advocates</h1>
+      </div>
       <br />
       <br />
       <Table initialAdvocates={initialAdvocates} initialTotalCount={totalCount} />
